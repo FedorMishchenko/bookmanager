6264faff077f4ebb6180547b94984f735f95f38e
Fedor Mishchenko <fedormishchenko.ua@gmail.com>
2019-03-12 23:33:29 +0200

    commit

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -1,24 +1,59 @@
 package dao;
 
+import common.SqlQueries;
 import entity.Order;
+import exceptions.MyDaoException;
+import repository.DBFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class OrderDao implements EntityDao<Order> {
     @Override
     public Order create(Order object) {
-        return null;
+        try (Connection connection = new DBFactory().getInstance().getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    SqlQueries.CREATE_ORDER);
+
+            return null;
+        }catch (SQLException e) {
+            throw new MyDaoException("SQLException in OrderDao create", e);
+        }
     }
 
     @Override
     public Order get(Order object) {
-        return null;
+        try (Connection connection = new DBFactory().getInstance().getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    SqlQueries.GET_ORDER);
+
+            return null;
+        }catch (SQLException e) {
+            throw new MyDaoException("SQLException in OrderDao get", e);
+        }
     }
 
     @Override
     public Order update(Order oldObject, Order newObject) {
-        return null;
+        try (Connection connection = new DBFactory().getInstance().getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    SqlQueries.UPDATE_ORDER);
+            
+            return null;
+        }catch (SQLException e) {
+            throw new MyDaoException("SQLException in OrderDao update", e);
+        }
     }
 
     @Override
     public void delete(Order object) {
+        try (Connection connection = new DBFactory().getInstance().getConnection()){
+            PreparedStatement statement = connection.prepareStatement(
+                    SqlQueries.DELETE_ORDER);
+
+        }catch (SQLException e) {
+            throw new MyDaoException("SQLException in OrderDao delete", e);
+        }
     }
 }
